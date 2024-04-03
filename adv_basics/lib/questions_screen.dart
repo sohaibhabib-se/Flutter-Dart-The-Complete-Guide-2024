bee5149de9f38dfdec7705c1d8db740ffd1bf050
Sohaib Habib <sohaibhabib8@gmail.com>
2024-04-04 01:37:44 +0500

    Quiz app lecture 69

--- adv_basics/lib/questions_screen.dart
+++ adv_basics/lib/questions_screen.dart
@@ -1,4 +1,7 @@
+import 'package:adv_basics/answer_button.dart';
+import 'package:adv_basics/data/questions.dart';
 import 'package:flutter/material.dart';
+import 'package:adv_basics/models/quiz_question.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -10,14 +13,26 @@ class QuestionsScreen extends StatefulWidget {
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
+
+  final currentQuestion = questions[0];
+
   @override
   Widget build(context) {
-    return Column(children: [
-      Text('Queston....'),
-      SizedBox(height: 30,),
-      ElevatedButton(onPressed: () {}, child: const Text("Answer 1")),
-      ElevatedButton(onPressed: () {}, child: const Text("Answer 1")),
-
-    ],);
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(currentQuestion.text, style: const TextStyle(color: Colors.white),),
+          SizedBox(
+            height: 30,
+          ),
+          AnswerButton(answerText: currentQuestion.answers[0], onTap: () {},),
+          AnswerButton(answerText: currentQuestion.answers[1], onTap: () {},),
+          AnswerButton(answerText: currentQuestion.answers[2], onTap: () {},),
+          AnswerButton(answerText: currentQuestion.answers[3], onTap: () {},),
+        ],
+      ),
+    );
   }
 }
