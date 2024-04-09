eb95b31c478aab294557c085de8e5993d6e45658
Sohaib Habib <sohaibhabib8@gmail.com>
2024-04-09 12:50:45 +0500

    Lecture75

--- adv_basics/lib/questions_screen.dart
+++ adv_basics/lib/questions_screen.dart
@@ -2,6 +2,7 @@ import 'package:adv_basics/answer_button.dart';
 import 'package:adv_basics/data/questions.dart';
 import 'package:flutter/material.dart';
 import 'package:adv_basics/models/quiz_question.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -13,7 +14,6 @@ class QuestionsScreen extends StatefulWidget {
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
-
   var currentQuestionIndex = 0;
 
   void answerQuestion() {
@@ -24,10 +24,8 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
     });
   }
 
-
   @override
   Widget build(context) {
-
     final currentQuestion = questions[currentQuestionIndex];
 
     return SizedBox(
@@ -40,7 +38,10 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
           children: [
             Text(
               currentQuestion.text,
-              style: const TextStyle(color: Colors.white),
+              style: GoogleFonts.lato(
+                  color: const Color.fromARGB(255, 201, 153, 251),
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold),
               textAlign: TextAlign.center,
             ),
             const SizedBox(
