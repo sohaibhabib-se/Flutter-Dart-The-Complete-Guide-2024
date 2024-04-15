bf5be5d916f8c4ff987926697db05584c615059c
Sohaib Habib <sohaibhabib8@gmail.com>
2024-04-16 01:41:58 +0500

    Lecture 83

--- adv_basics/lib/questions_screen.dart
+++ adv_basics/lib/questions_screen.dart
@@ -5,7 +5,9 @@ import 'package:adv_basics/models/quiz_question.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class QuestionsScreen extends StatefulWidget {
-  const QuestionsScreen({super.key});
+  const QuestionsScreen({super.key, required this.onSelectAnswer, });
+
+  final void Function(String answer) onSelectAnswer;
 
   @override
   State<QuestionsScreen> createState() {
@@ -16,7 +18,8 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   var currentQuestionIndex = 0;
 
-  void answerQuestion() {
+  void answerQuestion(String selectedAnswer) {
+    widget.onSelectAnswer(selectedAnswer);
     // currentQuestionIndex = currentQuestionIndex + 1;
     // currentQuestionIndex += 1;
     setState(() {
@@ -52,7 +55,9 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
             //   return AnswerButton(answerText: item, onTap: () {});
             // }),
             ...currentQuestion.getShuffledAnswers().map((item) {
-              return AnswerButton(answerText: item, onTap: answerQuestion);
+              return AnswerButton(answerText: item, onTap: () {
+                answerQuestion(item);
+              });
             }),
             // AnswerButton(answerText: currentQuestion.answers[0], onTap: () {},),
             // AnswerButton(answerText: currentQuestion.answers[1], onTap: () {},),
--- adv_basics/lib/quiz.dart
+++ adv_basics/lib/quiz.dart
@@ -1,6 +1,8 @@
 import 'package:adv_basics/questions_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:adv_basics/start_screen.dart';
+import 'package:adv_basics/data/questions.dart';
+import 'package:adv_basics/results_screen.dart';
 
 class Quiz extends StatefulWidget {
   const Quiz({super.key});
@@ -14,6 +16,7 @@ class Quiz extends StatefulWidget {
 class _QuizState extends State<Quiz> {
   // Widget? activeScreen;
   var activeScreen = 'start-screen';
+  List<String> selectedAnswers = [];
 
   // @override
   // void initState() {
@@ -30,6 +33,17 @@ class _QuizState extends State<Quiz> {
     });
   }
 
+  void chooseAnswer(String answer) {
+    selectedAnswers.add(answer);
+
+    if(selectedAnswers.length == questions.length) {
+      setState(() {
+        selectedAnswers = [];
+        activeScreen = 'result-screen';
+      });
+    }
+  }
+
   @override
   Widget build(context) {
 
@@ -37,7 +51,10 @@ class _QuizState extends State<Quiz> {
 
 
     if (activeScreen == 'questions-screen') {
-      screenWidget = const QuestionsScreen();
+      screenWidget = QuestionsScreen(onSelectAnswer: chooseAnswer,);
+    }
+    if (activeScreen == 'result-screen') {
+      screenWidget = ResultsScreen(chosenAnswers: selectedAnswers,);
     }
 
     return MaterialApp(
