28b407274f9ae13c4f1cf16e45456de6fca77e45
Sohaib Habib <sohaibhabib8@gmail.com>
2024-04-16 17:18:43 +0500

    Lecture 85

--- adv_basics/lib/questions_summary.dart
+++ adv_basics/lib/questions_summary.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class QuestionsSummary extends StatelessWidget {
@@ -14,6 +15,14 @@ class QuestionsSummary extends StatelessWidget {
         return Row(
           children: [
             Text(((data['question_index'] as int) + 1).toString()),
+            Expanded(
+              child: Column(children: [
+                Text(data['question'] as String),
+                const SizedBox(height: 5,),
+                Text(data['user_answer'] as String),
+                Text(data['correct_answer'] as String)
+              ],),
+            ),
           ],
         );
       }).toList(),
--- adv_basics/lib/results_screen.dart
+++ adv_basics/lib/results_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:adv_basics/data//questions.dart';
+import 'package:adv_basics/questions_summary.dart';
 
 class ResultsScreen extends StatelessWidget {
   const ResultsScreen({super.key, required this.chosenAnswers});
@@ -32,12 +33,12 @@ class ResultsScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('You answered X out of Y questions correctly!'),
-            SizedBox(
+            const Text('You answered X out of Y questions correctly!'),
+            const SizedBox(
               height: 30,
             ),
-            Text('List of answers and questions'),
-            SizedBox(
+            QuestionsSummary(getSummaryData()),
+            const SizedBox(
               height: 30,
             ),
             TextButton(onPressed: () {}, child: const Text('Restart Quiz'))
