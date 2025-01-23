8eb09d573a2174db942be56e9a92b09ee5bc191d
Muhammad Sohaib <m.sohaib@systemsltd.com>
2025-01-23 11:48:47 +0500

    Lecture 170

--- Section8/meals/lib/screens/categories.dart
+++ Section8/meals/lib/screens/categories.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:meals/models/category.dart';
+import 'package:meals/models/meal.dart';
 import 'package:meals/screens/meals.dart';
 import 'package:meals/widgets/category_grid_item.dart';
 
 import '../data/dummy_data.dart';
 
 class CatgoriesScreen extends StatelessWidget {
-  const CatgoriesScreen({super.key});
+  const CatgoriesScreen({super.key, required this.onToggleFavorite});
+
+  final void Function(Meal meal) onToggleFavorite;
 
   void _selectCategory(BuildContext context, Category category) {
     final filteredMeals = dummyMeals.where((meal) => meal.categories.contains(category.id)).toList();
     
-    Navigator.of(context).push(MaterialPageRoute(builder: (ctx) => MealsScreen(title: category.title, meals: filteredMeals)));//push(context, route);
+    Navigator.of(context).push(MaterialPageRoute(builder: (ctx) => MealsScreen(title: category.title, meals: filteredMeals, onToggleFavorite: onToggleFavorite,)));//push(context, route);
   }
 
   @override
--- Section8/meals/lib/screens/meal_details.dart
+++ Section8/meals/lib/screens/meal_details.dart
@@ -6,9 +6,11 @@ class MealDetailsScreen extends StatelessWidget {
   const MealDetailsScreen({
     super.key,
     required this.meal,
+    required this.onToggleFavorite,
   });
 
   final Meal meal;
+  final void Function(Meal meal) onToggleFavorite;
 
   @override
   Widget build(BuildContext context) {
@@ -17,7 +19,9 @@ class MealDetailsScreen extends StatelessWidget {
         title: Text(meal.title),
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: () {
+              onToggleFavorite(meal);
+            },
             icon: const Icon(Icons.star),
           ),
         ],
