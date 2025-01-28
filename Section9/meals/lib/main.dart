e2c7bf4ade269a80f6bfd3efb8dbfb5bb91dba9c
Muhammad Sohaib <m.sohaib@systemsltd.com>
2025-01-28 19:50:35 +0500

    Lecture 183 Section 9

---
+++ Section9/meals/lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+import 'package:google_fonts/google_fonts.dart';
+import 'package:meals/screens/categories.dart';
+import 'dart:io';
+
+import 'package:meals/screens/tabs.dart';
+
+class MyHttpOverrides extends HttpOverrides{
+  @override
+  HttpClient createHttpClient(SecurityContext? context){
+    return super.createHttpClient(context)
+      ..badCertificateCallback = (X509Certificate cert, String host, int port)=> true;
+  }
+}
+
+final theme = ThemeData(
+  useMaterial3: true,
+  colorScheme: ColorScheme.fromSeed(
+    brightness: Brightness.dark,
+    seedColor: const Color.fromARGB(255, 131, 57, 0),
+  ),
+  textTheme: GoogleFonts.latoTextTheme(),
+);
+
+void main() {
+  HttpOverrides.global = MyHttpOverrides();
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: theme,
+        home: const TabsScreen(),//const CatgoriesScreen(),
+    );
+  }
+}
