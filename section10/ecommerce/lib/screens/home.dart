4e4fae76c0ad6a2954f08c4332564f6e88d61733
Muhammad Sohaib <m.sohaib@systemsltd.com>
2025-02-14 12:09:43 +0500

    Section 11 completed

--- section10/ecommerce/lib/screens/home.dart
+++ section10/ecommerce/lib/screens/home.dart
@@ -1,8 +1,9 @@
+import 'package:ecommerce/models/grocery_item.dart';
 import 'package:ecommerce/screens/new_item.dart';
+import 'package:ecommerce/widgets/grocery_list_item.dart';
 import 'package:flutter/material.dart';
 
-import '../data/dummy_items.dart';
-import '../widgets/grocery_list_item.dart';
+// import '../data/dummy_items.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -12,26 +13,64 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final List<GroceryItem> _groceryItems = [];
 
-  void _addItem() {
-    Navigator.of(context).push(MaterialPageRoute(builder: (ctx) => const NewItemScreen()));
+  void _addItem() async {
+    final newItem = await Navigator.of(context).push<GroceryItem>(
+      MaterialPageRoute(
+        builder: (ctx) => const NewItemScreen(),
+      ),
+    );
+
+    if (newItem == null) {
+      return;
+    }
+
+    setState(() {
+      _groceryItems.add(newItem);
+    });
   }
-  
+
+  void _removeItem(GroceryItem item){
+    setState(() {
+      _groceryItems.remove(item);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    
+    Widget content = const Center(
+      child: Text(
+        'Please add new items using + button.',
+      ),
+    );
+
+    if (_groceryItems.isNotEmpty) {
+      content = ListView.builder(
+        itemCount: _groceryItems.length,
+        itemBuilder: (ctx, index) => Dismissible(
+          onDismissed: (direction) {
+            _removeItem(_groceryItems[index]);
+          },
+          key: ValueKey(_groceryItems[index].id),
+          child: GroceryListItem(
+            groceryItem: _groceryItems[index],
+          ),
+        ),
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Your Groceries"),
         actions: [
-          IconButton(onPressed: _addItem, icon: const Icon(Icons.add),),
+          IconButton(
+            onPressed: _addItem,
+            icon: const Icon(Icons.add),
+          ),
         ],
       ),
-      body: ListView.builder(
-        itemCount: groceryItems.length,
-        itemBuilder: (ctx, index) => GroceryListItem(groceryItem: groceryItems[index]),
-        ),
+      body: content,
     );
-    
   }
 }
--- section10/ecommerce/lib/screens/new_item.dart
+++ section10/ecommerce/lib/screens/new_item.dart
@@ -1,4 +1,6 @@
 import 'package:ecommerce/data/categories.dart';
+import 'package:ecommerce/models/category.dart';
+import 'package:ecommerce/models/grocery_item.dart';
 import 'package:flutter/material.dart';
 
 class NewItemScreen extends StatefulWidget {
@@ -9,6 +11,23 @@ class NewItemScreen extends StatefulWidget {
 }
 
 class _NewItemState extends State<NewItemScreen> {
+
+  final _formKey = GlobalKey<FormState>();
+  var _enterdName = '';
+  var _enteredQuantity = 1;
+  var _selectedCategory = categories[Categories.vegetables]!;
+
+  void _saveItem() {
+    if(_formKey.currentState!.validate()){
+      _formKey.currentState!.save();
+      print(_enterdName);
+      print(_enteredQuantity);
+      print(_selectedCategory.name);
+      Navigator.of(context).pop(GroceryItem(id: DateTime.now().toString(), name: _enterdName, quantity: _enteredQuantity, category: _selectedCategory));
+    }
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,6 +37,7 @@ class _NewItemState extends State<NewItemScreen> {
       body: Padding(
         padding: const EdgeInsets.all(12),
         child: Form(
+          key: _formKey,
           child: Column(
             children: [
               TextFormField(
@@ -26,7 +46,16 @@ class _NewItemState extends State<NewItemScreen> {
                   label: Text('Name'),
                 ),
                 validator: (value) {
-                  return 'Demo...';
+                  if (value == null || value.isEmpty || value.trim().length <= 1 || value.trim().length > 50) {
+                    return 'Must be between 1 and 50 characters.';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  // if(value == null) {
+                  //   return;
+                  // }
+                  _enterdName = value!;
                 },
               ),
               Row(
@@ -37,7 +66,17 @@ class _NewItemState extends State<NewItemScreen> {
                       decoration: const InputDecoration(
                         label: Text("Quantity"),
                       ),
+                      keyboardType: TextInputType.number,
                       initialValue: '1',
+                      validator: (value) {
+                        if (value == null || value.isEmpty || int.tryParse(value) == null || int.tryParse(value)! <= 0) {
+                          return 'Must be a valid positive number';
+                        }
+                        return null;
+                      },
+                      onSaved: (value) {
+                        _enteredQuantity = int.parse(value!);
+                      },
                     ),
                   ),
                   const SizedBox(
@@ -45,6 +84,7 @@ class _NewItemState extends State<NewItemScreen> {
                   ),
                   Expanded(
                     child: DropdownButtonFormField(
+                      value: _selectedCategory,
                       items: [
                         for (final category in categories.entries)
                           DropdownMenuItem(
@@ -62,7 +102,11 @@ class _NewItemState extends State<NewItemScreen> {
                             ]),
                           ),
                       ],
-                      onChanged: (value) {},
+                      onChanged: (value) {
+                        setState(() {
+                          _selectedCategory = value!;
+                        });
+                      },
                     ),
                   ),
                 ],
@@ -71,8 +115,10 @@ class _NewItemState extends State<NewItemScreen> {
               Row(
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
-                  TextButton(onPressed: () {}, child: const Text('Reset'),),
-                  ElevatedButton(onPressed: () {}, child: const Text('Add Item'),),
+                  TextButton(onPressed: () {
+                    _formKey.currentState!.reset();
+                  }, child: const Text('Reset'),),
+                  ElevatedButton(onPressed: _saveItem, child: const Text('Add Item'),),
                 ],
               )
             ],
